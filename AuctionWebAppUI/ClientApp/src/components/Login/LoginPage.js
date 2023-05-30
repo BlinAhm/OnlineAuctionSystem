396bab366238001c59b913a655da7b175210255d
aarbermorina <am51551@ubt-uni.net>
2023-05-30 12:42:43 +0200

    Login page

--- AuctionWebAppUI/ClientApp/src/components/Login/LoginPage.js
+++ AuctionWebAppUI/ClientApp/src/components/Login/LoginPage.js
@@ -1,1 +1,17 @@
-﻿const LoginPage()
+﻿import { Link } from 'react-router-dom';
+
+
+const LoginPage = () => {
+    return (
+        <form action="" method="post" id="formSignIn" className="LoginForm">
+            <div className="login-container">
+                <div className="login">
+
+                    <Link to="/sign-up">Don't have an account? Sign up!</Link>
+                </div>
+            </div>
+        </form>
+    );
+}
+
+export default LoginPage;
