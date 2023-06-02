d47346270a3bf64bd7b6ac3218ba975912ce121b
aarbermorina <am51551@ubt-uni.net>
2023-06-02 14:30:02 +0200

    login inputs

--- AuctionWebAppUI/ClientApp/src/components/Login/LoginInputs.js
+++ AuctionWebAppUI/ClientApp/src/components/Login/LoginInputs.js
@@ -1,1 +1,21 @@
-﻿
+﻿import './LoginInputs.css';
+
+const LoginInputs = () => {
+    return (
+        <div className="input-div">
+            <input name="email" id="SIemail" className="texts" type="email" placeholder="E-mail" />
+            <label id="labelSIEmail"></label>
+            <input name="password" id="SIpassword" className="texts" type="password" placeholder="Password" />
+            <label id="labelSIPassword"></label>
+
+            <div className="remember-me">
+                <div className="check">
+                    <input type="checkbox" /><p>Remember me</p>
+                </div>
+                <button disabled id="link-btn2">Forgot your password?</button>
+            </div>
+        </div>
+    );
+}
+
+export default LoginInputs;
