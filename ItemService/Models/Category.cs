6b53367d8ffe153a0282c38b3d980aff4783025e
stinaosmani <stinaosmani@gmail.com>
2023-06-07 22:53:02 +0200

    Added MongoDB and changed docker-compose

---
+++ ItemService/Models/Category.cs
@@ -0,0 +1,16 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace ItemService.Models
+{
+    [Serializable, BsonIgnoreExtraElements]
+    public class Category
+    {
+        [BsonId , BsonElement("category_id"), BsonRepresentation(BsonType.ObjectId)]
+        public int CategoryId { get; set; }
+
+        [BsonElement("category_name"), BsonRepresentation(BsonType.String)]
+        public string Name { get; set; }
+
+    }
+}
