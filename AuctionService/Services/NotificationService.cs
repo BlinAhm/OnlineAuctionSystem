102e93cb18a5bf892a7ddf47c6abc88d65fae566
aarbermorina <am51551@ubt-uni.net>
2023-06-13 17:43:03 +0200

    notif service

--- AuctionService/Services/NotificationService.cs
+++ AuctionService/Services/NotificationService.cs
@@ -1,6 +1,22 @@
-﻿namespace AuctionService.Services
+﻿using AuctionService.Models;
+
+namespace AuctionService.Services
 {
-    public class NotificationService
+    public class NotificationService : INotificationService
     {
+        public Task<bool> AddNotification(Notification notificationmodel)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> RemoveNotification(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> UpdateNotification(Notification updatemodel)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
