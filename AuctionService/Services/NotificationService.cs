e9c4b9b7a98be1bbb78475e03457655be12ea475
aarbermorina <am51551@ubt-uni.net>
2023-06-14 17:20:46 +0200

    notif service

--- AuctionService/Services/NotificationService.cs
+++ AuctionService/Services/NotificationService.cs
@@ -1,22 +1,51 @@
-﻿using AuctionService.Models;
+﻿using AuctionService.Data;
+using AuctionService.Models;
 
 namespace AuctionService.Services
 {
     public class NotificationService : INotificationService
     {
+        private readonly AuctionDbContext _context;
+
+        public NotificationService(AuctionDbContext context)
+        {
+            _context = context;
+        }
         public Task<bool> AddNotification(Notification notificationmodel)
         {
             throw new NotImplementedException();
         }
 
-        public Task<bool> RemoveNotification(int id)
+        public async Task<bool> RemoveNotification(int id)
         {
-            throw new NotImplementedException();
+            var notification = await _context.Notifications.FindAsync(id);
+            if (notification == null)
+                return false;
+            _context.Notifications.Remove(notification);
+            await _context.SaveChangesAsync();
+
+            if (await _context.Notifications.FindAsync(id) == null)
+                return true;
+            return false;
+
         }
 
-        public Task<bool> UpdateNotification(Notification updatemodel)
+        public async Task<bool> UpdateNotification(Notification updatemodel)
         {
-            throw new NotImplementedException();
+            Notification? notification = await _context.Notifications.FindAsync(updatemodel.Id);
+            if (notification == null)
+                return false;
+
+            notification.Id = updatemodel.Id;
+            notification.UserId = updatemodel.UserId;
+            notification.Message = updatemodel.Message;
+            notification.Date = updatemodel.Date;
+
+             await _context.SaveChangesAsync();
+
+            if (notification == await _context.Notifications.FindAsync(updatemodel.Id))
+                return true;
+            return false;
         }
     }
 }
