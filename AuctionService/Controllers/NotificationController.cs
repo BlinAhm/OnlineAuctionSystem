9e60c71275b9a993a55e768cb00e5208dd68354c
aarbermorina <am51551@ubt-uni.net>
2023-06-14 23:06:51 +0200

    update&remove notification

--- AuctionService/Controllers/NotificationController.cs
+++ AuctionService/Controllers/NotificationController.cs
@@ -1,6 +1,43 @@
-﻿namespace AuctionService.Controllers
+﻿using AuctionService.Data;
+using AuctionService.Models;
+using AuctionService.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AuctionService.Controllers
 {
-    public class NotificationsController
+    [Route("api/[controller]")]
+    [ApiController]
+    public class NotificationController : Controller
     {
+        private readonly INotificationService _notificationService;
+        private readonly AuctionDbContext _context;
+
+        public NotificationController(
+            INotificationService notificationService,
+            AuctionDbContext context
+            )
+        {
+            _notificationService = notificationService;
+            _context = context;
+        }
+        [HttpPut]
+        public async Task<IActionResult> UpdateNotification(Notification updateModel)
+        {
+            if (updateModel == null)
+                return StatusCode(StatusCodes.Status400BadRequest, new Response { Status = "Error", Message = "Update model invalid." });
+
+            if (await _notificationService.UpdateNotification(updateModel))
+                return Ok(new Response { Status = "Success", Message = "Notification updated successfully!" });
+            return StatusCode(StatusCodes.Status500InternalServerError, new Response { Status = "Error", Message = "Failed to update notification!" });
+        }
+
+        [HttpDelete]
+        [Route("{id}")]
+        public async Task<IActionResult> RemoveNotification(int id)
+        {
+            if (await _notificationService.RemoveNotification(id))
+                return Ok(new Response { Status = "Success", Message = "Notification deleted successfully!" });
+            return StatusCode(StatusCodes.Status500InternalServerError, new Response { Status = "Error", Message = "Failed to delete notification!" });
+        }
     }
 }
