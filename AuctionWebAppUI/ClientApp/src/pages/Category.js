afcb99ba441570a464221fd9cfb8574a4091c69f
aarbermorina <am51551@ubt-uni.net>
2023-06-17 15:23:44 +0200

    catrgory

--- AuctionWebAppUI/ClientApp/src/pages/Category.js
+++ AuctionWebAppUI/ClientApp/src/pages/Category.js
@@ -1,81 +1,34 @@
 ﻿
-import React, { useState } from 'react';
-    //import { Link } from 'react-router-dom';
-    import './css/CategoryPage.css'; 
+import { Link } from 'react-router-dom';
+import "./css/CategoryPage.css";
+const CategoryCard = (props) => {
+    return (
+        <Link to="" className="category-card">
+            <div className="category-image">
+                <img src="https://images.summitmedia-digital.com/esquiremagph/images/2021/03/03/cul-de-sac-the-podium.jpg" alt="img" />
+                </div>
+            <div className="category-details">
+                <h2>Clothes, Shoes</h2>
+            </div>
+        </Link>
 
+    );
 
+}
 const Category = () => {
-    const [selectedCategory, setSelectedCategory] = useState(null);
 
-    const categories = [
-        {
-            id: 1,
-            title: "Clothes, Shoes",
-            image: "https://thumbs.dreamstime.com/b/lululemon-hong-kong-china-january-clothes-display-store-new-town-plaza-shopping-mall-centre-sha-tin-174392453.jpg",
-        },
-        {
-            id: 2,
-            title: "Movies&DVD",
-            image: "https://media.karousell.com/media/photos/products/2023/4/23/46_dvd_movies_for_sale_as_one__1682217662_aa0f9579_progressive.jpg",
-        },
-        {
-            id: 3,
-            title: "Toys&Hobbies",
-            image: "https://resalvaged.com/wp-content/uploads/2021/01/hummel-figurines.jpeg",
-        }
-    ];
-
-    const handleCategoryClick = (categoryId) => {
-        setSelectedCategory(categoryId);
-    };
-
-    const handleCategoryMouseLeave = () => {
-        setSelectedCategory(null);
-    };
 
     return (
-        <div className="categories">
+        <div className="category-container">
             <h2>Categories</h2>
             <p>Choose an item category to bid!</p>
-            <div className="categories-container">
-                {categories.slice(0, 3).map((category) => (
-                    <div
-                        key={category.id}
-                        className="category"
-                        onMouseEnter={() => handleCategoryClick(category.id)}
-                        onMouseLeave={handleCategoryMouseLeave}
-                    >
-                        <div className="category-image">
-                            <img src={category.image} alt={category.title} />
-                        </div>
-                        <div className={`category-overlay ${selectedCategory === category.id ? 'active' : ''}`}>
-                            <h3>{category.title}</h3>
-                            <button>View More</button>
-                        </div>
-                    </div>
-                ))}
-                </div>
-            <div className="categories-container">
-                {categories.slice(3, 5).map((category) => (
-                    <div
-                        key={category.id}
-                        className="category"
-                        onMouseEnter={() => handleCategoryClick(category.id)}
-                        onMouseLeave={handleCategoryMouseLeave}
-                    >
-                        <div className="category-image">
-                            <img src={category.image} alt={category.title} />
-                        </div>
-                        <div className={`category-overlay ${selectedCategory === category.id ? 'active' : ''}`}>
-                            <h3>{category.title}</h3>
-                            <button>View More</button>
-                        </div>
-                    </div>
-                ))}
+            <div className="category-holder">
+                <CategoryCard />
             </div>
-            </div>
+        </div>
     );
-};
+
+}
 
 export default Category;
 
